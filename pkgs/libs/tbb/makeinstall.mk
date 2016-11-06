6f9fd1486836ad560d7a4acae2588f389a40acaa
zanton <zanton.zzz@gmail.com>
2016-11-06 22:34:41 +0900

    minor fix in pkgs/libs/tbb/makeinstall.mk

--- pkgs/libs/tbb/makeinstall.mk
+++ pkgs/libs/tbb/makeinstall.mk
@@ -1,7 +1,7 @@
 # Because TBB's top-level Makefile does not have "make install",
 # include this file at the end of ./src/Makefile so that PARSEC's parsecmgmt
 # can know how to install this TBB package into appropriate directories,
-# e.g., "include ../makeinstall.mk".
+# e.g., "include ${PARSECDIR}/pkgs/libs/tbb/makeinstall.mk".
 # This action should be taken every time a new TBB tarball is downloaded
 # and extracted into ./src by tbb.mk.
 
